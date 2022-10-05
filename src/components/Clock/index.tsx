3526266be0be8c97a1d2b7a2b8281778b05f69ce
George Baez <g.baez@gbh.com.do>
2022-10-05 08:31:40 -0400

    Create Clock component and style it

--- src/components/Clock/index.tsx
+++ src/components/Clock/index.tsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useReducer } from "react";
+import { ClockProps } from "../../interfaces";
+import { infoReducer, INITIAL_STATE } from "../../reducers/infoReducer";
+import "./style.scss";
 
-export  function Clock() {
+
+
+export function Clock({ time, country, countryCode, quote }: ClockProps) {
   return (
-    <div>Clock</div>
-  )
+    <div
+      className={`datetime__container ${
+        time && Number(time.split(":")[0]) >= 18 ? "night" : "day"
+      }`}
+    >
+      <div className='quote__container'>
+        <p className='quote__quote-item'>"{quote.quote}"</p>
+        <p className='quote__author-item'>{quote.author}</p>
+      </div>
+
+      <div className='clock__container'>
+        <div>
+          <div className='clock__greeting-item'>
+            {time && time >= "00:00" && time <= "11:59" ? (
+              <p>Good Morning</p>
+            ) : time && time >= "12:00" && time <= "18:00" ? (
+              <p>Good Afternoon</p>
+            ) : (
+              <p>Good Night</p>
+            )}
+          </div>
+
+          <p className='clock__time-item'>{time}</p>
+          <p className='clock__country-item'>
+            {country}, {countryCode}
+          </p>
+        </div>
+        <button>More</button>
+      </div>
+    </div>
+  );
 }
---
+++ src/interfaces/clock-props_interfaces.ts
@@ -0,0 +1,9 @@
+export type ClockProps = {
+  time: string | undefined;
+  country: string;
+  countryCode: string;
+  quote: {
+    author: string;
+    quote: string;
+  };
+};
