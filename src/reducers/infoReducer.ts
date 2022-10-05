076b29278f4fd87632fcb1e8a662c6d2b17a36bc
George Baez <g.baez@gbh.com.do>
2022-10-05 08:30:53 -0400

    Create services and reducer to serve the data

---
+++ src/reducers/infoReducer.ts
@@ -0,0 +1,56 @@
+import { ACTION_TYPES } from "../actions";
+
+const initialValues = {
+  data: "",
+  loading: false,
+  error: false
+};
+
+export const INITIAL_STATE = {
+  quote: {
+    ...initialValues
+  },
+  time: {
+    ...initialValues
+  },
+  ipNumber: {
+    ...initialValues
+  },
+  locationInfo: {
+    ...initialValues
+  }
+};
+
+export const infoReducer = (state, action) => {
+  switch (action.type) {
+    case ACTION_TYPES.FETCH_START:
+      return {
+        ...state,
+        [action.payload.name]: {
+          ...state[action.payload.name],
+          loading: true,
+          error: false
+        }
+      };
+    case ACTION_TYPES.FETCH_SUCCESS:
+      return {
+        ...state,
+        [action.payload.name]: {
+          data: action.payload.data,
+          loading: false,
+          error: false
+        }
+      };
+    case ACTION_TYPES.FETCH_ERROR:
+      return {
+        ...state,
+        [action.payload.name]: {
+          data: {},
+          loading: false,
+          error: true
+        }
+      };
+    default:
+      throw new Error();
+  }
+};
--- src/services/apisServices.ts
+++ src/services/apisServices.ts
@@ -1,5 +1,87 @@
-export class apisServices {
-    constructor() {}
-  
+import { ACTION_TYPES } from "../actions";
 
+type DispatchType = (args: {
+  type: string;
+  payload: { name: string; data: string } | { name: string };
+}) => void;
+
+export class ApisServices {
+  constructor() {}
+
+  async getUserPublicIp(dispatch: DispatchType) {
+    dispatch({ type: ACTION_TYPES.FETCH_START, payload: { name: "ipNumber" } });
+    try {
+      const response = await fetch("https://api.ipify.org?format=json");
+      const data = await response.json();
+      return dispatch({
+        type: ACTION_TYPES.FETCH_SUCCESS,
+        payload: { name: "ipNumber", data: data.ip }
+      });
+    } catch (error) {
+      return dispatch({
+        type: ACTION_TYPES.FETCH_ERROR,
+        payload: { name: "ipNumber" }
+      });
+    }
   }
+
+  async getTime(ip: string, dispatch: DispatchType) {
+    dispatch({ type: ACTION_TYPES.FETCH_START, payload: { name: "time" } });
+    if (ip !== "" && ip !== undefined) {
+      try {
+        const response = await fetch(`http://worldtimeapi.org/api/ip/${ip}`);
+        const data = await response.json();
+        return dispatch({
+          type: ACTION_TYPES.FETCH_SUCCESS,
+          payload: { name: "time", data: data }
+        });
+      } catch (error) {
+        return dispatch({
+          type: ACTION_TYPES.FETCH_ERROR,
+          payload: { name: "time" }
+        });
+      }
+    }
+  }
+
+  async getLocationWithIp(ip: string, dispatch: DispatchType) {
+    dispatch({
+      type: ACTION_TYPES.FETCH_START,
+      payload: { name: "locationInfo" }
+    });
+    try {
+      if (ip) {
+        const response = await fetch(`http://ip-api.com/json/${ip}`);
+        const data = await response.json();
+        return dispatch({
+          type: ACTION_TYPES.FETCH_SUCCESS,
+          payload: { name: "locationInfo", data: data }
+        });
+      }
+    } catch (error) {
+      return dispatch({
+        type: ACTION_TYPES.FETCH_ERROR,
+        payload: { name: "locationInfo" }
+      });
+    }
+  }
+
+  async getQuote(dispatch: DispatchType) {
+    dispatch({ type: ACTION_TYPES.FETCH_START, payload: { name: "quote" } });
+    try {
+      const response = await fetch(
+        "https://programming-quotes-api.herokuapp.com/Quotes/random"
+      );
+      const data = await response.json();
+      return dispatch({
+        type: ACTION_TYPES.FETCH_SUCCESS,
+        payload: { name: "quote", data: data }
+      });
+    } catch (error) {
+      return dispatch({
+        type: ACTION_TYPES.FETCH_ERROR,
+        payload: { name: "quote" }
+      });
+    }
+  }
+}
